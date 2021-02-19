bf34fd8f04f50749853bee608b965104f9cacbfb
luisaLuna <luisacasaslu@gmail.com>
2021-02-19 14:19:06 -0500

    Created Card Recipes

--- script.js
+++ script.js
@@ -42,3 +42,27 @@ function writeText() {
     setTimeout(writeText, speed)
 }
 // Here finishes the code for the animation on the h1 text.
+
+// Below is the code for the card recipes:
+
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2000)
+
+function getData() {
+    header.innerHTML = ' <img src="https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="Food"> ';
+    title.innerHTML = 'This is a deli recipe';
+    excerpt.innerHTML = ' This is the info of the recipe.';
+    name.innerHTML = 'Calories 100'; 
+    date.innerHTML = 'Time 20-min'; 
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
